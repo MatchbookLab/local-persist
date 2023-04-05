31fef48e72c277130280425b8686573188eabeab
Carbonique <ljnetwerk@posteo.nl>
2023-04-05 13:31:30 +0200

    add platform to ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 mkdir -p ./plugin/rootfs
 rm -rf ./plugin/rootfs/*
 docker buildx build --platform=$1 -t rootfsimage .
-id=$(docker create rootfsimage true) 
+id=$(docker create --platform=$1 rootfsimage true) 
 docker export "$id" | sudo tar -x -C ./plugin/rootfs
 
